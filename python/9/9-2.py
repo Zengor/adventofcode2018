1e5313aed9a173a8f475a0020da43e6f29ff0313
Unknown <zengor@hotmail.com>
2018-12-14 20:44:31 -0300

    Add python day 9

---
+++ python/9/9-2.py
@@ -0,0 +1,24 @@
+from collections import deque
+with open("input.txt") as f:
+    line = f.read().split()
+    num_players = int(line[0])
+    num_marbles = int(line[6]) * 100
+
+circle = deque([0])
+player_scores = [0] * num_players
+
+def curr_player(curr_turn):
+    return curr_turn % num_players
+
+for marble in range(1, num_marbles+1):
+    if marble % 23 == 0:
+        player = curr_player(marble)
+        circle.rotate(7)
+        player_scores[player] += marble + circle.pop()
+        circle.rotate(-1)
+    else:
+        circle.rotate(-1)
+        circle.append(marble)
+
+print(max(player_scores))
+
